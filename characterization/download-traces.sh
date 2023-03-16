167dea5c5b1cce2d63bb24eabfa82b51f971ee19
Jovan Stojkovic <jovans2@iacoma209.cs.illinois.edu>
2023-03-16 16:37:35 -0500

    add download

---
+++ characterization/download-traces.sh
@@ -0,0 +1,6 @@
+wget https://azurecloudpublicdataset2.blob.core.windows.net/azurepublicdatasetv2/azurefunctions_dataset2020/azurefunctions-accesses-2020.csv.bz2
+bzip2 -d azurefunctions-accesses-2020.csv.bz2
+
+sudo apt-get install unrar
+wget https://github.com/Azure/AzurePublicDataset/raw/master/data/AzureFunctionsInvocationTraceForTwoWeeksJan2021.rar
+unrar e AzureFunctionsInvocationTraceForTwoWeeksJan2021.rar
