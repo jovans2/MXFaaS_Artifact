1561d0ea769926a74b1d40339e3cf82357be1cd4
Jovan Stojkovic <jovans2@iacoma209.cs.illinois.edu>
2023-03-17 17:16:21 -0500

    add run-all

---
+++ KNative_prototype/run-all.py
@@ -0,0 +1,99 @@
+import subprocess
+import time
+import numpy as np
+import threading
+import requests
+from statistics import mean, median,variance,stdev
+
+# get the url of a function
+def getUrlByFuncName(funcName):
+    try:
+        output = subprocess.check_output("kn service describe " + funcName + " -vvv", shell=True).decode("utf-8")
+    except Exception as e:
+        print("Error in kn service describe == " + str(e))
+        return None
+    lines = output.splitlines()
+    for line in lines:
+        if "URL:"  in line:
+            url = line.split()[1]
+            return url
+
+output = subprocess.check_output("kn service list", shell=True).decode("utf-8")
+lines = output.splitlines()
+lines = lines[1:] # delete the first line
+
+services = []
+serviceNames = []
+
+for line in lines:
+    serviceName = line.split()[0] 
+    if serviceName not in serviceNames:
+        serviceNames.append(serviceName)
+
+for serviceName in serviceNames:
+    services.append(getUrlByFuncName(serviceName))
+
+def lambda_func(service):
+    global times
+    t1 = time.time()
+    r = requests.post(service, json={"name": "test"})
+    print(r.text)
+    t2 = time.time()
+    times.append(t2-t1)
+
+def EnforceActivityWindow(start_time, end_time, instance_events):
+    events_iit = []
+    events_abs = [0] + instance_events
+    event_times = [sum(events_abs[:i]) for i in range(1, len(events_abs) + 1)]
+    event_times = [e for e in event_times if (e > start_time)and(e < end_time)]
+    try:
+        events_iit = [event_times[0]] + [event_times[i]-event_times[i-1]
+                                         for i in range(1, len(event_times))]
+    except:
+        pass
+    return events_iit
+
+loads = [20, 50, 100]
+
+output_file = open("run-all-out.txt", "w")
+
+indR = 0
+for load in loads:
+    duration = 10
+    seed = 100
+    rate = load
+    # generate Poisson's distribution of events 
+    inter_arrivals = []
+    np.random.seed(seed)
+    beta = 1.0/rate
+    oversampling_factor = 2
+    inter_arrivals = list(np.random.exponential(scale=beta, size=int(oversampling_factor*duration*rate)))
+    instance_events = EnforceActivityWindow(0,duration,inter_arrivals)
+        
+    for service in services:
+        
+        threads = []
+        times = []
+        after_time, before_time = 0, 0
+
+        st = 0
+        for t in instance_events:
+            st = st + t - (after_time - before_time)
+            before_time = time.time()
+            if st > 0:
+                time.sleep(st)
+
+            threadToAdd = threading.Thread(target=lambda_func, args=(service, ))
+            threads.append(threadToAdd)
+            threadToAdd.start()
+            after_time = time.time()
+
+        for thread in threads:
+            thread.join()
+
+        print("=====================================", file=output_file, flush=True)
+        print(mean(times), file=output_file, flush=True)
+        print(median(times), file=output_file, flush=True)
+        print(np.percentile(times, 90), file=output_file, flush=True)
+        print(np.percentile(times, 95), file=output_file, flush=True)
+        print(np.percentile(times, 99), file=output_file, flush=True)
