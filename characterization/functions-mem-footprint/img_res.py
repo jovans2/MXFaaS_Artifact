287ed05e0cdc62c2b86b3122f9464bc29fc13f7b
Jovan Stojkovic <jovans2@iacoma209.cs.illinois.edu>
2023-03-31 00:06:12 -0500

    change imgRes

--- characterization/functions-mem-footprint/img_res.py
+++ characterization/functions-mem-footprint/img_res.py
@@ -16,17 +16,29 @@ connection_string = "DefaultEndpointsProtocol=https;AccountName=serverlesscache;
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 container_client = blob_service_client.get_container_client("artifacteval")
 
+images = {}
+for indI in range(0, 20):
+    blbName = "img" + str(indI) + ".png"
+    blob_client = BlobClient.from_connection_string(connection_string, container_name="artifacteval", blob_name=blbName)
+    with open(blbName, "wb") as my_blob:
+        download_stream = blob_client.download_blob()
+        my_blob.write(download_stream.readall())
+    images[blbName] = Image.open(blbName)
+
 memoryUse_old = memoryUse
 memoryUse = python_process.memory_info()[0]/2.**20  # memory use in MB
 print('memory use 2:', memoryUse-memoryUse_old, file=fileAppend)
 
 def main(params):
-    blobName = "img10.jpg"
-    blob_client = BlobClient.from_connection_string(connection_string, container_name="artifacteval", blob_name=blobName)
-    with open(blobName, "wb") as my_blob:
-        download_stream = blob_client.download_blob()
-        my_blob.write(download_stream.readall())
-    image = Image.open(blobName)
+    blobName = "img20.png"
+    if blobName not in images:
+        blob_client = BlobClient.from_connection_string(connection_string, container_name="artifacteval", blob_name=blobName)
+        with open(blobName, "wb") as my_blob:
+            download_stream = blob_client.download_blob()
+            my_blob.write(download_stream.readall())
+        image = Image.open(blobName)
+    else:
+        image = images[blobName]
     width, height = image.size
     # Setting the points for cropped image
     left = 4
@@ -34,11 +46,11 @@ def main(params):
     right = 100
     bottom = 3 * height / 5
     im1 = image.crop((left, top, right, bottom))
-    im1.save("newImage.jpeg")
+    im1.save("newImage.png")
 
-    fRead = open("newImage.jpeg","rb")
+    fRead = open("newImage.png","rb")
     value = fRead.read()
-    blobName = "img10_res.jpg"
+    blobName = "img10_res.png"
     blob_client = BlobClient.from_connection_string(connection_string, container_name="artifacteval", blob_name=blobName)
     blob_client.upload_blob(value, overwrite=True)
     return {"Image":"rotated"}
