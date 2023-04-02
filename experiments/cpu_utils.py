e66ebe9453ee0deceed5a09e462e9107d3f0590e
Jovan Stojkovic <jovans2@iacoma209.cs.illinois.edu>
2023-04-02 16:43:57 -0500

    changes

--- experiments/cpu_utils.py
+++ experiments/cpu_utils.py
@@ -12,7 +12,7 @@ ip_addresses = []
 
 def measure_cpu_util():
     while True:
-        time.sleep(2) 
+        time.sleep(1) 
         output = os.popen("docker stats " + service+  " --no-stream --format '{{.CPUPerc}}'").read()   
         flout = float(output[:-2])
         perc_cpu = max(0, flout-100)
@@ -22,7 +22,7 @@ containers = ["MXContainer", "Baseline"]
 for service in services:
     output = subprocess.check_output("docker run -d --name " + service + " --cpuset-cpus=0,1 jovanvr97/" + service, shell=True).decode("utf-8")
     time.sleep(5)
-    print("Start")
+    print("Start with " + containers[services.index(service)])
     threading.Thread(target=measure_cpu_util).start()
     client = docker.DockerClient()
     container = client.containers.get(service)
@@ -31,5 +31,10 @@ for service in services:
 
     time.sleep(50)
 
-    output = subprocess.check_output("docker stop " + service, shell=True).decode("utf-8")
-    output = subprocess.check_output("docker rm " + service, shell=True).decode("utf-8")
+    try:
+        output = subprocess.check_output("docker stop " + service, shell=True).decode("utf-8")
+        output = subprocess.check_output("docker rm " + service, shell=True).decode("utf-8")
+    except:
+        pass
+
+print("End experiment.")
